23ed67632cd1009978f6bca9fbdb11029346facb
Ethan4416 <predinchuk.e@outlook.com>
2024-12-03 14:04:11 -0800

    Working on cleaning code, not done yet

--- src/to_submit/naive_dft.m
+++ src/to_submit/naive_dft.m
@@ -14,9 +14,9 @@ function X = naive_dft(x)
     for k = 1:N
         for n = 1:N
             % Twiddle factor: W_N^(k-1)*(n-1)
-            twiddle = exp(-2i * pi * (k-1) * (n-1) / N);
+            W_n = exp(-2i * pi * (k-1) * (n-1) / N);
             % Accumulate the summation
-            X(k) = X(k) + x(n) * twiddle;
+            X(k) = X(k) + x(n) * W_n;
         end
     end
 end
