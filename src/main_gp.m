5fa5149ad0c876638ce6dab6e5e161119fc3f743
Gavin Pringle <gavinpringle00@gmail.com>
2024-12-02 17:18:12 -0800

    Completed main driver script and saved plots to figures folder

--- src/main_gp.m
+++ src/main_gp.m
@@ -27,7 +27,7 @@ N = max(size(clean_signal)); % Same for noisy signal
 % dif and dit ffts pad the input, the naive dft does not 
 N_fft = max(size(clean_signal_dit_fft));
 
-% Compute x axes in time and frequency domains 
+% Compute x axes in frequency domain
 f_dft = (-N/2:(N/2-1)) * (Fs / N); % Hz, fewer data points
 f_dif_dit = (-N_fft/2:(N_fft/2-1)) * (Fs / N) * (N / N_fft); % Hz
 
@@ -38,6 +38,7 @@ x_lim_left = 3540; x_lim_right  = 3580; % Zoomed in x domain
 linewidth = 3;
 font_size = 18;
 
+% Plot data
 subplot(2,2,1);
 hold on 
 plot(f_dif_dit, abs(clean_signal_dit_fft), 'Linewidth', linewidth);
@@ -98,3 +99,65 @@ ax.FontSize = font_size;
 % Computational cost analysis
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Create vector for different lengths of 2^8 to 2^20
+lengths = 2.^(5:14);
+num_tests = length(lengths);
+
+% Create output vector for the times and frequency for sinusoids
+dft_time = zeros(1, num_tests);
+dit_fft_time = zeros(1, num_tests);
+dif_fft_time = zeros(1, num_tests);
+sinusoid_frequency = 200;
+sampling_frequency = 1000;
+
+% For each size test the time it takes for the functions to run
+for i = 1:num_tests
+    current_length = lengths(i);
+    time = (0:current_length-1) / sampling_frequency;
+    sinusoid_signal = sin(2*pi*sinusoid_frequency*time);
+    noise = randn(1, current_length);
+    signal = sinusoid_signal + noise;
+
+    % start time for naive_dft
+    tic;
+    naive_dft(signal(:));
+    naive_dft_time(i) = toc;
+
+    % start time for dit_fft
+    tic;
+    dit_fft(signal(:));
+    dit_fft_time(i) = toc;
+
+    % start time for dit_fft
+    tic;
+    dif_fft(signal(:));
+    dif_fft_time(i) = toc;
+end
+
+disp("Lengths size: " + size(lengths));
+disp("Naive DFT time size: " + size(naive_dft_time));
+disp("DIF FFT time size: " + size(dif_fft_time));
+disp("DIT FFT time size: " + size(dit_fft_time));
+
+% Plot signal length vs time
+f2 = figure(2);
+% loglog(lengths, naive_dft_time, lengths, dif_fft_time, lengths, dit_fft_time, "--");
+loglog(lengths, dit_fft_time,   "o-", ...  % DIT FFT
+       lengths, dif_fft_time,  "o--", ...  % DIF FFT
+       lengths, naive_dft_time, "o:", ...  % Naive DFT
+       'Linewidth', 2);     
+
+% Update x-axis to log base 2
+set(gca, 'XScale', 'log', 'YScale', 'log'); % Use log scale
+xticks(lengths); % Tick marks only at powers of 2
+xticklabels("2^{"+log2(lengths)+"}"); % Label ticks as log2(lengths)
+
+xlabel("Signal Input Size (Log Base 2)");
+ylabel("Execution Time (s)");
+legend("DIT FFT", "DIF FFT", "Naive DFT", 'Location', 'northwest');
+title("Time comparison for Naive DFT vs DIF FFT vs DIT FFT");
+grid on;
+% Add grid lines to improve readability
+grid minor;
+ax = gca;
+ax.FontSize = 12;
